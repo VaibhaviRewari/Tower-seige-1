a96fb41128a5c4590bb5ef5b49c17d0d6390a1ce
VaibhaviRewari <78912700+VaibhaviRewari@users.noreply.github.com>
2021-05-08 14:45:17 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,30 @@
+class Block{
+
+constructor(x, y, width, height) {
+
+var options = {
+friction :1.0,
+restitution :0.4
+} 
+
+this.body =Bodies.rectangle(x, y, width, height, options);
+this.width = width;
+this.height= height;
+World.add(world, this.body);
+}
+
+//display funtion
+display(){
+
+var angle = this.body.angle; 
+var pos= this.body.position;
+
+push();
+translate(pos.x, pos.y);
+rotate(angle); rectMode(CENTER);
+rect(0,6, this.width, this.height);
+
+pop();
+
+}
+}
